7eaedea4d12d9064ede8dc7f32c83cc16679d0f3
blazewtt <ahmad.ahmapro2@gmail.com>
2023-01-11 23:50:30 +0100

    password_hash()

--- Classes/User.php
+++ Classes/User.php
@@ -20,7 +20,7 @@ class User
         $statement = $pdo->prepare('INSERT INTO users (email, password, firstname, lastname, role) VALUES (:email, :password, :first_name, :last_name, :role)');
         $statement->execute([
             'email' => $this->email,
-            'password' => $this->password,
+            'password' => password_hash($this->password, PASSWORD_DEFAULT),
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'role' => $this->role,
@@ -32,18 +32,18 @@ class User
         return false;
     }
 
+
     public function login()
     {
         global $pdo;
-        $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email AND password = :password');
-        $statement->execute([
-            'email' => $this->email,
-            'password' => $this->password,
-        ]);
+        $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+        $statement->execute(['email' => $this->email]);
         $user = $statement->fetchObject('User');
         if ($user) {
-            $_SESSION['user'] = $user;
-            return true;
+            if (password_verify($this->password, $user->password)) {
+                $_SESSION['user'] = $user;
+                return true;
+            }
         }
         return false;
     }
